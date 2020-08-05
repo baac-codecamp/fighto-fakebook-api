6d46184177cb1186fd90faf53ec3b58bb7e1c1ee
WanitchT <wanitch.t@gmail.com>
2020-08-05 11:15:12 +0700

    add cors module

--- models/guideModel2.js
+++ models/guideModel2.js
@@ -2,29 +2,34 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
-const userSchema = Schema({
+const guideSchema = Schema({
   
   password: { type: String, required: true, trim: true , minlength: 3 },
   firstname:  { type: String, required: true, trim: true },
   lastname:  { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true, unique: true, index: true },
+  gender:  { type: String, required: true, trim: true },
+  address:  { type: String, required: true, trim: true },
+  education:  { type: String, required: true, trim: true },
+  displayname:  { type: String, required: true, trim: true },
+  datecreate : {type: String, required: true, trim: true }
 },{
   collection: 'users'
 });
 
-userSchema.methods.encryptPassword = async (password)  => {
+guideSchema.methods.encryptPassword = async (password)  => {
   const salt = await bcrypt.genSalt(10);
   const hashPassword = await bcrypt.hash(password, salt);
   return hashPassword;
 }
 
-userSchema.methods.comparePassword = async function (password)  {
+guideSchema.methods.comparePassword = async function (password)  {
   console.log(password);
   console.log(this.password);
   const isValid = await bcrypt.compare(password, this.password);
   return isValid;
 }
   
-  const user = mongoose.model('User', userSchema);
+  const guide = mongoose.model('Guide', guideSchema);
   
-  module.exports = user;;
+  module.exports = guide;
